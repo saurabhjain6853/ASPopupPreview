7b9d087e6fbbcf53fc5f836e67c9bfc9b2cb3409
Saurabh Jain <saurabh.jain6853@gmail.com>
2021-01-22 21:35:19 +0530

    Add the preview action

--- ASPopupPreviewExample/PreviewVC.swift
+++ ASPopupPreviewExample/PreviewVC.swift
@@ -19,6 +19,10 @@ class PreviewVC: UIViewController {
     var imgString = ""
     var delegate:PreviewDelegate!
     
+    override var previewActionItems: [UIPreviewActionItem] {
+        return previewActions()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -34,5 +38,25 @@ class PreviewVC: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         delegate.previewWillHide()
+    }
+    
+    func previewActions() -> [UIPreviewActionItem] {
+        let actionDetail = UIPreviewAction.init(title: "View Details", style: .default) { (action, vcRefrence) in
+            print("Tapped")
+        }
+        
+        let makeOffer = UIPreviewAction.init(title: "Make Offer", style: .default) { (action, vcRefrence) in
+            print("Tapped")
+        }
+        
+        let actionMessage = UIPreviewAction.init(title: "Message", style: .default) { (action, vcRefrence) in
+            print("Tapped")
+        }
+        
+        let share = UIPreviewAction.init(title: "Share", style: .default) { (action, vcRefrence) in
+            print("Tapped")
+        }
+        
+        return [actionDetail, makeOffer, actionMessage, share]
     }
 }
